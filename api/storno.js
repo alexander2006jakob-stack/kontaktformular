8a990d007fecada7a7fb7b48fd4e7d62f64f8601
alexander2006jakob-stack <alexander.2006.jakob@gmail.com>
2026-05-04 22:49:27 +0200

    Update storno.js

--- api/storno.js
+++ api/storno.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
                 from: "onboarding@resend.dev",
                 to: "test.p1.alexjakob@gmail.com",  
                 subject: "Termin storniert",
-                html: `<p>Storno für Token: ${token}</p>`
+                text: `Storno für Token: ${token}`
             })
         });
 
